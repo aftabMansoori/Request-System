330b5eafaef9f90c968fbafc98d1e29e9f172659
aftabMansoori <mohammed.aftab18@siesgst.ac.in>
2022-08-03 02:06:56 +0530

    updated the global css and login page

--- src/Pages/Login.vue
+++ src/Pages/Login.vue
@@ -1,44 +1,49 @@
 <template>
   <main>
-    <form>
+    <section>
       <div class="text-center mb-2">
         <h1>Login</h1>
         <p class="text-secondary">Please enter your credentials to continue</p>
       </div>
-      <div class="demo-input-size">
-        <div class="my-1">
-          <label class="text-secondary" for="email">Your email</label>
-          <el-input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="name@email.com"
-            class="my bg"
-            v-model="credentials.email"
-            prefix-icon="el-icon-message"
-          />
-        </div>
-        <div class="my-1">
-          <label class="text-secondary" for="password">Your Password</label>
-          <el-input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="atleast 8 characters"
-            v-model="credentials.password"
-            class="my"
-            prefix-icon="el-icon-lock"
-            show-password
-          />
-        </div>
+      <form>
+        <div class="demo-input-size">
+          <div class="my-1">
+            <label class="text-secondary" for="email">Your email</label>
+            <el-input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="name@email.com"
+              class="my bg"
+              v-model="credentials.email"
+              prefix-icon="el-icon-message"
+            />
+          </div>
+          <div class="my-1">
+            <label class="text-secondary" for="password">Your Password</label>
+            <el-input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="atleast 8 characters"
+              v-model="credentials.password"
+              class="my"
+              prefix-icon="el-icon-lock"
+              show-password
+            />
+          </div>
 
-        <el-button type="primary" class="w-100 my-1" round>Login</el-button>
+          <el-button type="primary" class="w-100 my-1" round>Login</el-button>
 
-        <p class="text-center">
-          Don't have account? <i class="text-primary">Sign Up</i>
-        </p>
-      </div>
-    </form>
+          <p class="text-center">
+            Don't have account?
+            <router-link to="/register"
+              ><i class="text-primary">Sign Up</i></router-link
+            >
+          </p>
+        </div>
+      </form>
+    </section>
   </main>
 </template>
 
@@ -63,18 +68,9 @@ main {
   display: flex;
   align-items: center;
   justify-content: center;
-  flex: auto;
 }
 
 section {
-  width: 50%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-form {
   width: 400px;
 }
 </style>
