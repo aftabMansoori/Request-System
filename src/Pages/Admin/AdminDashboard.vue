96d0644be78b1cef3c0d62a9b73e93c844a86325
aftabMansoori <mohammed.aftab18@siesgst.ac.in>
2022-08-05 02:58:31 +0530

    ---admin dashboard

--- src/Pages/Admin/AdminDashboard.vue
+++ src/Pages/Admin/AdminDashboard.vue
@@ -1,6 +1,22 @@
 <template>
   <section>
-    <h1 class="text-primary">Dashboard</h1>
+    <div class="header">
+      <h1 class="text-primary mb-none">Dashboard</h1>
+      <small class="text-secondary">View your activity and stats here</small>
+    </div>
+
+    <div>
+      <div class="stats">
+        <article>
+          <h5 class="text-secondary">Number of Leaves Requested</h5>
+          <p>27</p>
+        </article>
+        <article>
+          <h5 class="text-secondary">Number of Videos Requested</h5>
+          <p>32</p>
+        </article>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -12,8 +28,48 @@ export default {
 
 <style scoped>
 section {
+  padding: 1em 3em;
+}
+
+.stats {
   display: flex;
-  flex-direction: column;
-  padding: 0.7em 1em;
+  flex-wrap: wrap;
+  margin-top: 3em;
+}
+
+.stats > article {
+  width: 250px;
+  height: 80px;
+  margin-inline-end: 2em;
+  padding: 1em 2em;
+  border-radius: 7px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+
+.stats > article:nth-child(1) p {
+  color: rgb(245, 108, 108);
+  font-size: 3em;
+  font-weight: 600;
+}
+.stats > article:nth-child(2) p {
+  color: rgb(230, 162, 60);
+  font-weight: 600;
+  font-size: 3em;
+}
+
+@media (max-width: 768px) {
+  section {
+    padding: 1em;
+    overflow: auto;
+  }
+  .header {
+    text-align: center;
+    margin-bottom: 2em;
+  }
+
+  .stats > article {
+    width: 100%;
+    margin: 1em 0.5em;
+  }
 }
 </style>
