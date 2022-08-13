f7d017e0003b5e07de886d3bada6584e44baa0e5
aftabMansoori <mohammed.aftab18@siesgst.ac.in>
2022-08-13 22:11:59 +0530

    added toast notification

--- src/plugins.js
+++ src/plugins.js
@@ -4,12 +4,16 @@ import VueRouter from "vue-router";
 
 import Vuelidate from "vuelidate";
 import ElementUI from "element-ui";
+import Toast from "vue-toastification";
 
 import "element-ui/lib/theme-chalk/index.css";
 import "./global.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import locale from "element-ui/lib/locale/lang/en";
+import "vue-toastification/dist/index.css";
 // import "./darkmode.css";
+
+Vue.config.lang = "en";
 
 Vue.use(Vuex);
 Vue.use(VueRouter);
@@ -17,4 +21,8 @@ Vue.use(VueRouter);
 Vue.use(Vuelidate);
 Vue.use(ElementUI, { locale });
 
-Vue.config.lang = "en";
+Vue.use(Toast, {
+  transition: "Vue-Toastification__fade",
+  maxToasts: 20,
+  newestOnTop: true,
+});
